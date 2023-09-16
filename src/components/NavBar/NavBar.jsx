347b91bfebec9c18215d13a50ad5b08dfbad84ba
Victor Sepulveda <victorseva123@gmail.com>
2023-09-16 17:04:34 -0500

    Actualizado 16/09/23

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,17 +3,20 @@ import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar"
 import PATHROUTES from "../../helpers/PathRoutes.helper";
 import styles from "./NavBar.module.css";
+
 //import Favorites from "../Favorites/Favorites";
 
 const NavBar = (props) => {
     const {onSearch} =  props;
   return (
-    <div className={styles.nav}>
+    <><div className={styles.nav}>
       <Link to={PATHROUTES.HOME}>Home</Link>
       <Link to={PATHROUTES.ABOUT}>About</Link>
       <Link to={PATHROUTES.FAVORITES}>Favorites</Link>
-        <SearchBar onSearch={onSearch} />
     </div>
+    <div className={styles.navi}>
+    <SearchBar onSearch={onSearch} />
+    </div></>
   )
 }
 
