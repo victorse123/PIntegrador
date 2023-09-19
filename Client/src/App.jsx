7fce582b835aa2a9d0f64ad817688b3f5c45c695
Victor Sepulveda <victorseva123@gmail.com>
2023-09-19 06:02:29 -0500

    Actualizado 19/09

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -46,7 +46,7 @@ function App() {
 
 
    function onSearch(id) {
-      axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
+      axios(`http://localhost:3001/rickandmorty/character/${id}`).then(({ data }) => {
          if (data.name) {
             setCharacters((oldChars) => [...oldChars, data]);
          } else {
